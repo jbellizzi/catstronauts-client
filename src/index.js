12198f52b8361d27e41002d3aa3cf917a3ea2fc7
John Bellizzi <jabellizzi406@gmail.com>
2023-12-09 10:42:11 -0500

    update server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000", // change to YOUR own production server
+  uri: "https://catstronauts-server-production-5468.up.railway.app/",
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
